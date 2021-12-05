f9bdab63f2e951be75315229aa0d7c5d83307fbf
dedknd <50517002+dedknd@users.noreply.github.com>
2021-12-05 14:36:16 +0100

    Create main.c

---
+++ 2021/04/part1/main.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+
+#if 1
+#define INPUT "input.txt"
+#define DRAW_SIZE 27 
+#define BOARDS 3
+#else
+#define INPUT "input2.txt"
+#define DRAW_SIZE 100
+#define BOARDS 100
+#endif
+
+#define BOARD_ROWS 5
+#define BOARD_COLS 5
+#define BOARD_SIZE (BOARD_ROWS * BOARD_COLS)
+
+typedef enum {
+    FALSE,
+    TRUE
+} boolean;
+
+typedef struct {
+    int value;
+    boolean mark;
+} entry;
+
+typedef struct {
+    boolean bingo;
+    int board;
+    int score;
+} game;
+
+void parse_file(char *filename, int *draw_number, int *drawCount, entry *board_array, int *boardCount);
+game bingoCheck(entry *board_array, int length, int draw);
+
+int main(void)
+{
+    int draw_number[DRAW_SIZE];
+    entry board_array[BOARD_SIZE * BOARDS];
+    int drawCount = 0;
+    int boardCount = 0;
+
+
+    parse_file(INPUT, draw_number, &drawCount, board_array, &boardCount);
+
+
+    for (int i = 0; i < drawCount; i++)
+    {
+        int length = boardCount * BOARD_SIZE;
+
+        game result = bingoCheck(board_array, length, draw_number[i]);
+
+        if (result.bingo)
+        {
+            printf("score: %d\nboard number: %d\n", result.score, result.board);
+            return 0;
+        }
+    }
+}
+
+void parse_file(char *filename, int *draw_number, int *drawCount, entry *board_array, int *boardCount)
+{
+    
+    FILE *file = fopen(filename, "r");
+    char c;
+    int value = 0;
+    int boardIndex = 0;
+
+    boolean DRAW_NUMBER_PARSE = TRUE;
+    boolean BOARD_PARSE = FALSE;
+    boolean PARSING_NUMBER = FALSE;
+
+    while ((c = getc(file)) != EOF)
+    {
+        if (DRAW_NUMBER_PARSE)
+        {
+            if (c != ',' && c != '\n')
+            {
+                value = value * 10 + (c - '0');        
+                draw_number[*drawCount] = value;
+            }
+            else
+            {
+                value = 0;
+                (*drawCount)++;
+            }
+        }
+
+        if (c == '\n' && DRAW_NUMBER_PARSE)
+        {
+            DRAW_NUMBER_PARSE = FALSE;
+            BOARD_PARSE = TRUE;
+        }
+
+        if (BOARD_PARSE)
+        {
+            // read boards into one array 
+            // each board starts at boardIndex - boardIndex % BOARD_SIZE 
+            if (c != ' ' && c != '\n')
+            {
+                if (!PARSING_NUMBER)
+                {
+                    PARSING_NUMBER = TRUE;
+                }
+                value = value * 10 + (c - '0');        
+            }
+            // PARSING_NUMBER for catching double whitespace
+            else if ((c == ' ' || c == '\n') && PARSING_NUMBER)
+            {
+                PARSING_NUMBER = FALSE;
+                board_array[boardIndex].value = value;
+                board_array[boardIndex].mark = FALSE;
+                boardIndex++;
+                value = 0;
+            }
+        }
+    }
+    *boardCount = boardIndex / BOARD_SIZE;
+}
+
+game bingoCheck(entry *board_array, int length, int draw)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (board_array[i].value == draw)
+        {
+            board_array[i].mark = TRUE;
+
+            // check current board for bingo
+            int col = i % BOARD_COLS; // relative column
+            int index = i - (i % BOARD_SIZE); // absolute index for current board
+            int row = (i - col) - index; // relative row
+
+            entry *board = &board_array[index]; 
+            int rowChecksum = 0;
+            int colChecksum = 0;
+            for (int j = 0; j < BOARD_ROWS; j++)
+            {
+                // check column for BINGO 
+                if (board[col + BOARD_COLS * j].mark == TRUE)
+                {
+                    colChecksum += 1;
+                }
+
+                if (colChecksum == 5)
+                {
+                    int score = 0;
+                    for (int k = 0; k < BOARD_SIZE; k++)
+                    {
+                        if (board[k].mark == FALSE)
+                        {
+                            score += board[k].value;
+                        }
+                    }
+                    score *= draw;
+                    int winnerBoard = i / 25 + 1;
+                    game result = {TRUE, score, winnerBoard};
+                    return result;
+                }
+            }
+
+            // check row for BINGO
+            for (int j = 0; j < BOARD_COLS; j++)
+            {
+                if (board[row + j].mark == TRUE)
+                {
+                    rowChecksum += 1;
+                }
+
+                if (rowChecksum == 5)
+                {
+                    int score = 0;
+                    for (int k = 0; k < BOARD_SIZE; k++)
+                    {
+                        if (board[k].mark == FALSE)
+                        {
+                            score += board[k].value;
+                        }
+                    }
+                    score *= draw;
+                    int winnerBoard = i / 25 + 1;
+                    game result = {TRUE, score, winnerBoard};
+                    return result;
+                }
+            }
+        }
+    }
+    game result = {FALSE, 0, 0};
+    return result;
+}
